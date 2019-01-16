b852fd5398e2d70fa57c1e518aa3070155dcc6cf
Maud Gilet <maud.gilet@gmail.com>
2019-01-16 13:26:46 +0100

    methods to load and store data

--- stock.rb
+++ stock.rb
@@ -1,12 +1,16 @@
+require 'csv'
+
 class Stock
   attr_accessor :numbers
 
   def initialize
     @numbers = []
+    load_csv
   end
 
   def push(number)
     @numbers << number
+    save_to_csv(number)
   end
 
   def pop
@@ -19,5 +23,17 @@ class Stock
 
   def mean
     @numbers.sum / @numbers.length
+  end
+
+  def load_csv
+    CSV.foreach('stock.csv') do |row|
+      @numbers << row.to_i
+    end
+  end
+
+  def save_to_csv(number)
+    CSV.open('stock.csv', 'w') do |csv|
+      csv << number
+    end
   end
 end
